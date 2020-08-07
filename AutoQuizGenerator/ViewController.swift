1e34d8e1dcdbced10b491ecde37e3a41f5cb9949
DOALA0155 <kyleshouki@gmail.com>
2020-08-07 17:06:49 +0900

    Initial Commit

---
+++ AutoQuizGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoQuizGenerator
+//
+//  Created by 勝山翔紀 on 2020/08/07.
+//  Copyright © 2020 勝山翔紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
